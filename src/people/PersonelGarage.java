797fb2d31565306056464ec87f30bfd24733587f
Morghen <remy.mauhin@gmail.com>
2017-03-27 15:14:48 +0200

    personnel

---
+++ src/people/PersonelGarage.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package people;
+import authenticate.*;
+
+/**
+ *
+ * @author Rémy
+ */
+public abstract class PersonelGarage extends Personne implements Identifiable,AValider {
+    
+    private String matricule;
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+    
+    @Override
+    public boolean isValid()
+    {
+        return this.validate();
+    }
+    
+    @Override
+    public boolean validate()
+    {
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
