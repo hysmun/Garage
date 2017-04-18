affd679fe96ede1604872e285b62a7ffbc497021
hysmun <throdlemassif@hotmail.com>
2017-04-18 13:44:12 +0200

    Merge origin/master

---
+++ src/mainPackages/mainGarage.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainPackages;
+
+/**
+ *
+ * @author ante
+ */
+public class mainGarage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("TEST\n");
+        loginForm loginWindows = new loginForm();
+        loginWindows.setLocation(500, 400);
+        loginWindows.setVisible(true);
+    }
+    
+}
