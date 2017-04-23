d099885fca8120e5adb8871e69be27ec99dab690
hysmun <throdlemassif@hotmail.com>
2017-04-23 16:07:59 +0200

    Merge origin/master

--- src/mainPackages/mainGarage.java
+++ src/mainPackages/mainGarage.java
@@ -8,6 +8,9 @@ package mainPackages;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import people.Client;
+import vehicules.TypeVoiture;
+import vehicules.Voiture;
+import vehicules.*;
 
 /**
  *
@@ -15,36 +18,51 @@ import people.Client;
  */
 public class mainGarage {
 
-    /**
-     */
-    public static Client cliTest1 = new Client("1", "Paul", "Icier", "rue du cimetière 42", "0496100811");
-    public static Client cliTest2 = new Client("2", "Toma", "Te", "rue des 100 dre", "0496100811");
-    public static Client cliTest3 = new Client("3", "truc", "Icier", "rue du cimetière 42", "0496100811");
+    public static Client cliTest1;
+        public static Client cliTest2;
+        public static Client cliTest3;
+    
+        public static Voiture voiTest1 ;
+        public static Voiture voiTest2 ;
+        public static Voiture voiTest3;
+    
     
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.print("TEST\n");
-        loginForm loginWindows = new loginForm();
-        loginWindows.setLocation(500, 400);
-        loginWindows.setVisible(true);
-        
-        
-        while(loginWindows.loginValue < 1)
+        try
         {
-            try {
-                //attente
-                //System.out.print("t\n");
-                Thread.sleep(10);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(mainGarage.class.getName()).log(Level.SEVERE, null, ex);
+            // TODO code application logic here
+            System.out.print("TEST\n");
+            loginForm loginWindows = new loginForm();
+            loginWindows.setLocation(500, 400);
+            loginWindows.setVisible(true);
+            cliTest1 = new Client("1", "Paul", "Icier", "rue du cimetière 42", "0496100811");
+            cliTest2 = new Client("2", "Toma", "Te", "rue des 100 dre", "0496100811");
+            cliTest3 = new Client("3", "truc", "Bidule", "rue lle", "0496100811");
+
+            voiTest1 = new Voiture("1", new TypeVoiture("Peugeot", "berline", 5), "1-NLU-134", cliTest1);
+            voiTest2 = new Voiture("2", new TypeVoiture("Peugeot", "berline", 5), "1-NLU-134", cliTest1);
+            voiTest3 = new Voiture("3", new TypeVoiture("Peugeot", "berline", 5), "1-NLU-134", cliTest1);
+
+            while(loginWindows.loginValue < 1)
+            {
+                try {
+                    //attente
+                    //System.out.print("t\n");
+                    Thread.sleep(10);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(mainGarage.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
+            System.out.print("Login reussi !");
+            loginWindows.setVisible(false);
+
+            applicationGestionForm formPrincipale = new applicationGestionForm();
+            formPrincipale.setLocation(500, 400);
+            formPrincipale.setVisible(true);
         }
-        System.out.print("Login reussi !");
-        loginWindows.setVisible(false);
-        
-        applicationGestionForm formPrincipale = new applicationGestionForm();
-        formPrincipale.setLocation(500, 400);
-        formPrincipale.setVisible(true);
+        catch(Throwable e)
+        {
+            System.out.println("Error main garage !!!");
+        }
     }
-    
 }
