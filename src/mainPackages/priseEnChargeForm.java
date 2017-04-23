e8626daae6c2e7f953b816f6d9a4a488a7f37143
hysmun <throdlemassif@hotmail.com>
2017-04-23 21:26:07 +0200

    Merge origin/master

--- src/mainPackages/priseEnChargeForm.java
+++ src/mainPackages/priseEnChargeForm.java
@@ -49,6 +49,11 @@ public class priseEnChargeForm extends javax.swing.JFrame {
 
         priseEnChargePontRadBut.setSelected(true);
         priseEnChargePontRadBut.setText("Pont");
+        priseEnChargePontRadBut.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                priseEnChargePontRadButMouseClicked(evt);
+            }
+        });
 
         priseEnChargeTable.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -80,6 +85,11 @@ public class priseEnChargeForm extends javax.swing.JFrame {
         priseEnChargePontComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3" }));
 
         priseEnChargeSolRadBut.setText("Sol");
+        priseEnChargeSolRadBut.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                priseEnChargeSolRadButMouseClicked(evt);
+            }
+        });
 
         priseEnChargeOkButton.setText("Ok");
         priseEnChargeOkButton.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -89,6 +99,11 @@ public class priseEnChargeForm extends javax.swing.JFrame {
         });
 
         priseEnChargeAnnulerButton.setText("Annuler");
+        priseEnChargeAnnulerButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                priseEnChargeAnnulerButtonMouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -143,9 +158,31 @@ public class priseEnChargeForm extends javax.swing.JFrame {
     private void priseEnChargeOkButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_priseEnChargeOkButtonMouseClicked
         if(priseEnChargeTable.getSelectedRowCount() == 0)
         {
-            
+            if(priseEnChargePontRadBut.isSelected())
+            {
+                //on va le mettre sur un des ponts 
+            }
+            else if(priseEnChargeSolRadBut.isSelected())
+            {
+                //on va le pose sur le sol 
+            }
         }
     }//GEN-LAST:event_priseEnChargeOkButtonMouseClicked
+
+    private void priseEnChargeAnnulerButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_priseEnChargeAnnulerButtonMouseClicked
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_priseEnChargeAnnulerButtonMouseClicked
+
+    private void priseEnChargePontRadButMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_priseEnChargePontRadButMouseClicked
+        // TODO add your handling code here:
+        priseEnChargeSolRadBut.setSelected(false);
+    }//GEN-LAST:event_priseEnChargePontRadButMouseClicked
+
+    private void priseEnChargeSolRadButMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_priseEnChargeSolRadButMouseClicked
+        // TODO add your handling code here:
+        priseEnChargePontRadBut.setSelected(false);
+    }//GEN-LAST:event_priseEnChargeSolRadButMouseClicked
 
     /**
      * @param args the command line arguments
