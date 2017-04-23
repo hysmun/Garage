286eba6403f86990ab3bdf5dff1900ed1d462bc2
hysmun <throdlemassif@hotmail.com>
2017-04-23 10:48:26 +0200

    Merge origin/master

--- src/login/hashtableLogin.java
+++ src/login/hashtableLogin.java
@@ -4,12 +4,17 @@ import java.util.Hashtable;
 
 public class hashtableLogin {
     
-    private Hashtable<String, String> loginTable;
+    private final Hashtable<String, String> loginPersonnelTable;
+    private final Hashtable<String, String> loginTable;
     
     public hashtableLogin() {
-        loginTable = new Hashtable();
-        loginTable.put("root","root");
-        loginTable.put("guest","guest");
+        loginPersonnelTable = new Hashtable();
+        loginPersonnelTable.put("root","root");
+        loginPersonnelTable.put("guest","guest");
+        
+        loginMembreTable = new Hashtable();
+        loginMembreTable.put("root","root");
+        loginMembreTable.put("guest","guest");
     }
     
     
