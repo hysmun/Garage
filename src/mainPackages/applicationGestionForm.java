3404276b24557405abf7cdbc529aa9d72cc54690
hysmun <throdlemassif@hotmail.com>
2017-04-23 18:05:40 +0200

    Merge origin/master

--- src/mainPackages/applicationGestionForm.java
+++ src/mainPackages/applicationGestionForm.java
@@ -283,6 +283,7 @@ public class applicationGestionForm extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void appGestionToutPresentRadButMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_appGestionToutPresentRadButMouseClicked
