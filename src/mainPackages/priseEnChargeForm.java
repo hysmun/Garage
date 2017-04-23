9a97bf750dbf724422f5a1a39e924330ae15f921
Morghen <remy.mauhin@gmail.com>
2017-04-23 19:43:20 +0200

    Merge origin/master
    Conflicts:
    	src/mainPackages/RDVForm.form
    	src/mainPackages/RDVForm.java

--- src/mainPackages/priseEnChargeForm.java
+++ src/mainPackages/priseEnChargeForm.java
@@ -48,9 +48,24 @@ public class priseEnChargeForm extends javax.swing.JFrame {
 
             },
             new String [] {
-
+                "Type voiture", "Immatriculation", "Proprietaire", "Travail", "Remarques"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         jScrollPane2.setViewportView(priseEnChargeTable);
 
         priseEnChargePontComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3" }));
