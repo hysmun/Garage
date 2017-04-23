2497f7f75412363fbc2049d56b8da1e609043901
Morghen <remy.mauhin@gmail.com>
2017-04-23 13:51:08 +0200

    adds

--- src/login/hashtableLogin.java
+++ src/login/hashtableLogin.java
@@ -4,17 +4,23 @@ import java.util.Hashtable;
 
 public class hashtableLogin {
     
-    private final Hashtable<String, String> loginPersonnelTable;
-    private final Hashtable<String, String> loginTable;
+    private final Hashtable<String, String> loginMembreTable;
+    private final Hashtable<String, String> loginExternTable;
     
     public hashtableLogin() {
-        loginPersonnelTable = new Hashtable();
-        loginPersonnelTable.put("root","root");
-        loginPersonnelTable.put("guest","guest");
-        
         loginMembreTable = new Hashtable();
         loginMembreTable.put("root","root");
-        loginMembreTable.put("guest","guest");
+        
+        loginExternTable = new Hashtable();
+        loginExternTable.put("guest","guest");
+    }
+
+    public Hashtable<String, String> getLoginMembreTable() {
+        return loginMembreTable;
+    }
+
+    public Hashtable<String, String> getLoginExternTable() {
+        return loginExternTable;
     }
     
     
