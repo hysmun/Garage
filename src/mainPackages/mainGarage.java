06e5cd317d98d43d99f462de74bb353cb0caa96b
hysmun <throdlemassif@hotmail.com>
2017-05-15 14:37:25 +0200

    modif générale

--- src/mainPackages/mainGarage.java
+++ src/mainPackages/mainGarage.java
@@ -5,8 +5,11 @@
  */
 package mainPackages;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -31,9 +34,15 @@ public class mainGarage {
         try
         {
             // TODO code application logic here
+            String current = System.getProperty("user.dir");
+            File fileProperties = new File(filePropertiesGeneral);
+            if(!fileProperties.exists())
+            {
+                OutputStream ot = new FileOutputStream(filePropertiesGeneral);
+            }
+            FileInputStream filePropertiesInput = new FileInputStream(fileProperties);
+            generalProperties.load(filePropertiesInput);
             
-            FileInputStream input = new FileInputStream(filePropertiesGeneral);
-            generalProperties.load(input);
 
             loginForm loginWindows = new loginForm();
             loginWindows.setVisible(true);
