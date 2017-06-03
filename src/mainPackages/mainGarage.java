ac45fb18358d92f8c593154ca81386a55d00f1a9
hysmun <throdlemassif@hotmail.com>
2017-06-03 13:01:53 +0200

    modif générale

--- src/mainPackages/mainGarage.java
+++ src/mainPackages/mainGarage.java
@@ -28,6 +28,8 @@ public class mainGarage {
     
     public static String fileListe = "fileListe.save";
     public static String filePropertiesGeneral = "general.properties";
+    public static String currentDir;
+    public static String propertiesDir;
     
     public static Properties generalProperties = new Properties();
     public static Properties clientProperties = new Properties();
@@ -37,7 +39,7 @@ public class mainGarage {
         {
             // TODO code application logic here
             //gestion properties
-            String current = System.getProperty("user.dir");
+            currentDir = System.getProperty("user.dir");
             File fileProperties = new File(filePropertiesGeneral);
             if(!fileProperties.exists())
             {
@@ -47,28 +49,41 @@ public class mainGarage {
                 generalProperties.setProperty("fichier-pneu-properties", "pneu.properties");
                 generalProperties.setProperty("fichier-piece-properties", "piece.properties");
                 generalProperties.setProperty("fichier-lubifiant-properties", "lubrifiant.properties");
+                generalProperties.store(ot, null);
             }
             else
             {
                 InputStream it = new FileInputStream(filePropertiesGeneral);
                 generalProperties.load(it);
+            }
+            System.out.println("filePropertiesGeneral charger");
+            
+            File userdir = new File(currentDir);
+            propertiesDir = generalProperties.getProperty("dossier-properties") + System.getProperty("file.separator");
+            File dossierProperties = new File(generalProperties.getProperty("dossier-properties"));
+            if(!dossierProperties.exists())
+            {
+                //dossier inexistant
+                dossierProperties.mkdir();
             }
             
-            File ClientfileProperties = new File(generalProperties.getProperty("fichier-client-properties"));
+            File ClientfileProperties = new File(propertiesDir+generalProperties.getProperty("fichier-client-properties"));
             if(!ClientfileProperties.exists())
             {
-                OutputStream ot = new FileOutputStream(filePropertiesGeneral);
-                generalProperties.setProperty("dossier-properties", "Properties");
-                generalProperties.setProperty("fichier-client-properties", "client.properties");
-                generalProperties.setProperty("fichier-pneu-properties", "pneu.properties");
-                generalProperties.setProperty("fichier-piece-properties", "piece.properties");
-                generalProperties.setProperty("fichier-lubifiant-properties", "lubrifiant.properties");
+                OutputStream ot = new FileOutputStream(propertiesDir+generalProperties.getProperty("fichier-client-properties"));
+                clientProperties.setProperty("dossier-properties", "Properties");
+                clientProperties.setProperty("fichier-client-properties", "client.properties");
+                clientProperties.setProperty("fichier-pneu-properties", "pneu.properties");
+                clientProperties.setProperty("fichier-piece-properties", "piece.properties");
+                clientProperties.setProperty("fichier-lubifiant-properties", "lubrifiant.properties");
+                clientProperties.store(ot, null);
             }
             else
             {
-                InputStream it = new FileInputStream(filePropertiesGeneral);
-                generalProperties.load(it);
+                InputStream it = new FileInputStream(propertiesDir+generalProperties.getProperty("fichier-client-properties"));
+                clientProperties.load(it);
             }
+            System.out.println("fichier-client-properties");
             
             
 
@@ -89,6 +104,10 @@ public class mainGarage {
             
             applicationGestionForm formPrincipale = new applicationGestionForm();
             formPrincipale.setVisible(true);
+        }
+        catch(NullPointerException e)
+        {
+            System.out.println("ERREUR POINTEUR NULL");
         }
         catch(IOException e)
         {
